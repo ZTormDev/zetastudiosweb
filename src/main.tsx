50dbc04a5d2b547f5f07b459bd3a3ed97ffa34d2
ZTormDev <160201272+ZTormDev@users.noreply.github.com>
2024-10-22 01:40:13 -0300

    a

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.scss'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import ZetaStudios from "./ZetaStudios";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Voxeland from "./games-pages/Voxeland/Voxeland";
+import GameInfo from "./games-pages/Voxeland/Pages/GameInfo/GameInfo";
+import News from "./games-pages/Voxeland/Pages/News/News";
+import BlogDetail from "./games-pages/Voxeland/Pages/blogs/BlogDetail";
+import Support from "./games-pages/Voxeland/Pages/Support/Support";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ZetaStudios />, // Página principal
+  },
+  {
+    path: "/voxeland",
+    element: <Voxeland />, // Página de Voxeland
+  },
+  {
+    path: "/voxeland/gameinfo",
+    element: <GameInfo />, // Página de GameInfo
+  },
+  {
+    path: "/voxeland/news",
+    element: <News />, // Página de Noticias
+  },
+  {
+    path: "/voxeland/news/:id",
+    element: <BlogDetail />, // Página de detalles del blog
+  },
+  {
+    path: "/voxeland/support",
+    element: <Support />, // Página de Soporte
+  },
+]);
+
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
